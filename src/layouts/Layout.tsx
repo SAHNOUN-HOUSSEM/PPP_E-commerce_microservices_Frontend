9123d12676b98ed8c0e38eb0f64d3503988422b7
FkihMehdi <mehdi.fkih@insat.ucar.tn>
2024-05-09 21:34:07 +0100

    fetch category from backend

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "../components";
 
 const Layout = () => {
   return (
     <>
-      <h1>before</h1>
+      <Navbar />
       <Outlet />
       <h1>after</h1>
     </>

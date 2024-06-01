09c0b9b4a85ef4fa8473917d264f05f3d13ce5d4
L2RR2L <91757694+L2RR2L@users.noreply.github.com>
2024-06-01 17:17:31 +0100

    added order admin component (#6)

--- src/components/OrderAdmin.tsx
+++ src/components/OrderAdmin.tsx
@@ -1,7 +1,61 @@
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export const OrderAdmin = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8081/orders").then((response) => {
+      console.log("response from order admin", response.data);
+      setOrders(response.data);
+    });
+  }, []);
   return (
-    <div>
-      <h1>OrderAdmin</h1>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Order ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Product Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Quantity
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) =>
+              Object.entries(order.products).map(
+                ([productName, quantity], index) => (
+                  <tr
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    key={`${order.id}-${index}`}
+                  >
+                    {index === 0 && (
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        rowSpan={Object.keys(order.products).length}
+                      >
+                        {order.orderID}
+                      </th>
+                    )}
+                    <td className="px-6 py-4">{productName}</td>
+                    <td className="px-6 py-4">{quantity}</td>
+                  </tr>
+                )
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+      {/* <Outlet /> */}
     </div>
   );
 };
